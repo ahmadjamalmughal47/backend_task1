5ab3be0ea0533f23846e9f307a1388a6ec2fcdd6
ahmadjamalmughal47 <ahmadjamalmughal47@gmail.com>
2021-09-16 19:39:33 +0500

    put added and reduced boilerplate

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const PORT = 3000;
+app.listen(PORT, () => {console.log(`listening on port ${PORT}`)});
+
 let myArray = [
     {"name": "item 1", "id": 1, },
     {"name": "item 2", "id": 2, },
@@ -11,18 +14,14 @@ let myArray = [
 app.get("/", (req, res  ) => {
     res.send("empty get request blah blah blah");
 })
-const PORT = 3000;
-app.listen(PORT, () => {console.log(`listening on port ${PORT}`)});
+
 
 app.get("/api/array", (req, res) => {
     res.send(myArray)
 })
 
-app.get("/api/array/:id/:name", (req, res) => {
-    const value = myArray.find(current => current.id === parseInt(req.params.id) && current.name === req.params.name);
-    if(!value) {
-        res.status(404).send("value not found");
-    }
+app.get("/api/array/:id", (req, res) => {
+    const value = updateValue(req, res);
     res.send(value);  
 })
 
@@ -49,3 +48,18 @@ app.post("/api/array", (req, res)=> {
     res.send(`${object.id} and ${object.name} \nwas added.`);
 })
 
+app.put("/api/array/:id", (req, res) => {
+    // refactor
+    const value = updateValue(req, res);
+
+    value.name = req.body.name;
+    res.send(value)
+})
+
+function updateValue(req, res) {
+    const value = myArray.find(current => current.id === parseInt(req.params.id));
+    if (!value) {
+        res.status(404).send("value not found");
+    }
+    return value;
+}
